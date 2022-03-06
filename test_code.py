3683824888aae6ab9f371b0ba27891df1c007689
feifan.ma <feifan.ma@nio.com>
2022-03-06 22:32:15 +0800

    代码上传

--- test_code.py
+++ test_code.py
@@ -1,29 +1,6 @@
-import sys
-from pyspark.sql import SparkSession, Window, SQLContext
-from pyspark.sql import functions
-from dateutil.parser import parse
-from pyspark.sql.types import *
-from pyspark import StorageLevel
-import pandas as pd
-import numpy as np
-import json
-import datetime
-import time
-from py_saas_algorithm.alarm import gen_alarm_id, gen_hash_code
-from py_saas_algorithm.recorder import ErrorRecorder
-from py_saas_algorithm.algorithm import Algorithm
-from py_saas_algorithm.data_source import read_battery_data
-from collections import defaultdict
-from py_saas_algorithm.recorder import ErrorRecorder
-from py_saas_algorithm import proxima
+import tushare as ts
 
-spark = SparkSession.builder \
-    .appName("AnalysingBatteryAlarms") \
-    .config("spark.default.parallelism", "600") \
-    .config("spark.sql.shuffle.partitions", "600") \
-    .enableHiveSupport() \
-    .getOrCreate()  # prod
-sql_context = SQLContext(spark)
+print(ts.get_hist_data('600848')) #一次性获取全部数据
 
 
-
+ts.get_hist_data('600848',start='2015-01-05',end='2015-01-09')
